82717ea6aead8018847d92e35f8c44c78b1b6e3f
BAN <bagas.nugroho@equity.id>
2021-01-21 09:02:59 +0700

    templete done 99%

--- src/pages/AdminPages/LoginAdmin.jsx
+++ src/pages/AdminPages/LoginAdmin.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { MDBIcon } from 'mdbreact';
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Style  from '../../assets/AdminAssets/cssModule/LoginAdmin.module.css'
 export const LoginAdmin = () => {
+
+    const history = useHistory();
+
+    const LoginAction=  ()=>{
+          useEffect(() => {
+       
+          history.push("/admin/home");
+        
+      }, [LoginAction]);
+    
+    }
     return (
         <div className={Style.containerLogin}>
-            <form action="LoginData">
+            <form action={LoginAction}>
                 <div className={Style.cardStyle}>
                     <div className="imgWraper">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Logo-equity-life.jpg/1200px-Logo-equity-life.jpg" alt=""/>
@@ -18,11 +31,8 @@ export const LoginAdmin = () => {
                         <MDBIcon className="fa-lg" icon="key" />
                         <input name="usrename" type="password" placeholder="Password"/>
                     </div>
-                    </div>
-                    
-
-
-                    <button type="submit" className="btn btn-success">Login</button>
+                    </div>                    
+            <button onClick={LoginAction} className="btn btn-success">Login</button>
                 </div>
             </form>
             
